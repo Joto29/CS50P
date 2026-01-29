7018d4b30413986fb8924db3bdcbfaa1323f3d85
Johan Tovar <jo833057@ucf.edu>
2026-01-29 09:28:44 -0500

    Solved problem set 0.4.1

--- einstein.py
+++ einstein.py
@@ -2,8 +2,7 @@ def main():
     mass = int(input("m: "))
     print(convert_e(mass))
 
-def convert_e(mass):
-    c = 300000000
+def convert_e(mass, c = 300000000):
     e = mass * (c**2)
     return e
 
