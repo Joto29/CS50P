6bc0b651a67364d5f3c66ce09e6ac19492b69c69
Johan Tovar <jo833057@ucf.edu>
2026-01-29 09:19:27 -0500

    Solved problem 0.3.1

---
+++ faces.py
@@ -0,0 +1,15 @@
+def main():
+    # ask for input
+    word = input()
+    print(convert(word))
+
+def convert(new_word):
+    #convert icon
+    new_word = new_word.replace(":)" , "🙂")
+    new_word = new_word.replace(":(" , "🙁")
+    return new_word
+
+main()
+
+
+
