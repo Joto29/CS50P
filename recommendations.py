b17ca8d708a1608d42255955447d7644866f2f00
Johan Tovar <jo833057@ucf.edu>
2026-01-30 11:59:16 -0500

    New Practice

--- recommendations.py
+++ recommendations.py
@@ -1,23 +1,23 @@
 def main():
-    difficulty = input("Difficult or casual? ")
-    players = input("Multiplayer or Single-player? ")
-
-    if difficulty == "Difficult":
-        if players == "Multiplayer":
-            recommend("Poker")
-        elif players == "Single-player":
-            recommend("Klondike")
-        else:
-            print("Enter a valid number of players")
-    elif difficulty == "Casual":
-        if players == "Multiplayer":
-            recommend("Hearts")
-        elif players == "Single-player":
-            recommend("clock")
-        else:
-            print("Enter a valid number of players")
-    else:
+    difficulty = input("Difficult or Casual? ")
+    if not (difficulty == "Difficult" or difficulty == "Casual"):
         print("Enter a valid difficulty")
+        return
+    
+    players = input("Multiplayer or Single-player? ")
+    if not (players == "Multiplayer" or players == "Single-player"):
+        print("Enter a valid number of players")
+        return
+
+    if difficulty == "Difficult" and players == "Multiplayer":
+            recommend("Poker")
+    elif difficulty == "Difficult" and players == "Single-player":
+            recommend("Klondike")
+    elif difficulty == "Casual" and players == "Multiplayer":
+            recommend("Hearts")
+    else:
+          recommend("Clock")
+
 
 def recommend(game):
     print("You might like", game)
