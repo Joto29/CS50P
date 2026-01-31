2dbff1d0499157b7ec0df2a0a61b6e558aa86928
Johan Tovar <jo833057@ucf.edu>
2026-01-31 11:43:16 -0500

    Solved problem 1.2

---
+++ extensions.py
@@ -0,0 +1,21 @@
+def main():
+    #prompt the user for file
+    file_inp = input("File name: ").lower()
+
+    #check conditions 
+    if file_inp.endswith(".gif"):
+        print("image/gif")
+    elif file_inp.endswith(".jpg") or file_inp.endswith(".jpeg"):
+        print("image/jpeg")
+    elif file_inp.endswith(".png"):
+        print("image/png")
+    elif file_inp.endswith(".pdf"):
+        print("application/pdf")
+    elif file_inp.endswith(".txt"):
+        print("text/plain")
+    elif file_inp.endswith(".zip"):
+        print("application/zip")
+    else:
+        print("application/octet-stream")
+
+main()
