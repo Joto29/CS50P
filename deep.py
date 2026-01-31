2b8590213346b360313662168d2daa80fbb66953
Johan Tovar <jo833057@ucf.edu>
2026-01-31 11:02:16 -0500

    Solved problem 1.0

---
+++ deep.py
@@ -0,0 +1,10 @@
+def main():
+    #prompt the user
+    user_prompt = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ").lower()
+
+    if not (user_prompt == "42" or user_prompt == "forty-two" or user_prompt == "forty two"):
+        print("No")
+    else:
+        print("Yes")
+
+main()
