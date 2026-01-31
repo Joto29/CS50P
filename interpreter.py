a84d966d9f10d0300d9e66c12959e610048524e4
Johan Tovar <jo833057@ucf.edu>
2026-01-31 12:01:44 -0500

    Solved Problem 1.3

---
+++ interpreter.py
@@ -0,0 +1,21 @@
+def main():
+    exp_inp = input("Expression: ")
+
+    x , y, z= exp_inp.split(" ")
+    new_x = int(x)
+    new_z = int(z)
+
+    if y == "+":
+        total = new_x + new_z
+        print(float(total))
+    elif y == "-":
+        total = new_x - new_z
+        print(float(total))
+    elif y == "*":
+        total = new_x * new_z
+        print(float(total))
+    else:
+        total = new_x / new_z
+        print(round(total,1))
+
+main()
