d34eafc34eaea0fee087e59dba7a9b5d09a9bcf2
Johan Tovar <jo833057@ucf.edu>
2026-02-02 11:29:02 -0500

    new updates

---
+++ coke.py
@@ -0,0 +1,14 @@
+def main():
+    amount_due = 50
+    coins = [25 , 10 , 5]
+
+    while amount_due > 0:
+        print(f"Amount Due: {amount_due}")
+        coin = int(input("Insert Coin: "))
+
+        if coin in coins:
+            amount_due -= coin
+    print(f"Change Owed: {amount_due}")
+
+
+main()
