c93d9c4d784485b64f4a7acb377b9cb97d155d24
Johan Tovar <jo833057@ucf.edu>
2026-02-02 09:34:31 -0500

    new updates

---
+++ bee.py
@@ -0,0 +1,23 @@
+WORDS =  {"PAIR": 4, "HAIR": 4, "CHAIR": 5, "GRAPHIC": 7}
+
+def main():
+    print("Welcome to Spelling Bee!")
+    for word, points in WORDS.items():
+        print(f"{word} was worth {points} points.")
+    '''
+    print("Your letters are: A I P C R H G")
+    '''
+    ''''
+    while len(WORDS) > 0:
+        print(f"{len(WORDS)} words left!")
+        guess = input("Guess a word: ")
+
+        # TODO: Check if guess in dictionary
+        if guess == "GRAPHIC":
+            WORDS.clear()
+            print("You've won1!")
+        if guess in WORDS.keys():
+            points = WORDS.pop(guess)
+            print(f"Good job! you score {points} points.")
+    '''
+main()
---
+++ camel.py
@@ -0,0 +1,16 @@
+def main():
+    camel_word = input("caseCamel: ")
+    return convert(camel_word)
+    ...
+
+def convert(s):
+    new_string = ""
+    for c in s:
+        if c.isupper():
+            new_string +="_"
+            new_string += c.lower()  
+        else:
+            new_string += c        
+    return print(f"snake_case: {new_string}")
+
+main()
