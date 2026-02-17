c6b66eb91bd25bb57baff5bc20aa5d0e1dcdae4a
Johan Tovar <jo833057@ucf.edu>
2026-02-17 09:41:48 -0500

    practice

---
+++ Assignments/emojize.py
@@ -0,0 +1,13 @@
+import emoji
+
+#ask the user for input
+def main():
+    inp = input("Input: ")
+    print(convert_inp(inp))
+    
+
+#convert the input
+def convert_inp(inp):
+    return (emoji.emojize(f"Output: {inp}", language="alias" , variant="text_type"))
+
+main()
